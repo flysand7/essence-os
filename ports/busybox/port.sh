c177a38dc5ceb94edb0f9b56391ae95fa3ea1508
Colin Davidson <colrdavidson@gmail.com>
2022-01-22 06:19:26 -0800

    Fix busybox port for OSX

--- ports/busybox/port.sh
+++ ports/busybox/port.sh
@@ -1,10 +1,18 @@
 set -e
+
 SYSROOT=`realpath root`
 VERSION=1.33.1
+
+uname -a | grep Darwin > /dev/null
+if [ $? -ne 1 ]; then
+	PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
+fi
+
 bin/build get-source-checked 12cec6bd2b16d8a9446dd16130f2b92982f1819f6e1c5f5887b6db03f5660d28 busybox-$VERSION https://www.busybox.net/downloads/busybox-$VERSION.tar.bz2
 cd bin/source
 cp ../../ports/busybox/config .config
 sed -i "51 i CONFIG_SYSROOT=\"$SYSROOT\"" .config
+
 make -j `nproc`
 cp busybox $SYSROOT/Applications/POSIX/bin
 cp LICENSE ../BusyBox\ License.txt
