2a0b3e0a125635b018a96599fccf3fff3dad0c15
nakst <>
2022-01-23 09:29:31 +0000

    fix busybox on linux

--- ports/busybox/port.sh
+++ ports/busybox/port.sh
@@ -3,10 +3,7 @@ set -e
 SYSROOT=`realpath root`
 VERSION=1.33.1
 
-uname -a | grep Darwin > /dev/null
-if [ $? -ne 1 ]; then
-	PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
-fi
+uname -a | grep Darwin && PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
 
 bin/build get-source-checked 12cec6bd2b16d8a9446dd16130f2b92982f1819f6e1c5f5887b6db03f5660d28 busybox-$VERSION https://www.busybox.net/downloads/busybox-$VERSION.tar.bz2
 cd bin/source
