f4b640dd0d25804ae3bdb6123471dc1cad25efcc
nakst <>
2022-02-03 19:23:17 +0000

    move other automation scripts in start.script

--- util/automation/build_default.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eux
-
-cd essence
-mkdir -p bin root
-echo "accepted_license=1" >> bin/build_config.ini
-echo "automated_build=1"  >> bin/build_config.ini
-./start.sh get-toolchain
-./start.sh build
