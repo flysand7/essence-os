a20b6fe91f9a2d7d2672af91590edb3ed1cd7ef1
nakst <>
2022-03-24 17:02:53 +0000

    start.sh: revert changes

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-cd "$(dirname "$0")" && mkdir -p bin && gcc -o bin/script util/script.c -g -Wall -Wextra -O2 -pthread && bin/script util/start.script options="`echo $@`"
+cd "$(dirname "$0")" && mkdir -p bin && gcc -o bin/script util/script.c -g -Wall -Wextra -O2 -pthread -ldl && bin/script util/start.script options="`echo $@`"
